5c8ff50cf86a68a50ded29386454171cb2bfb0c8
Brad <bycarlson23@gmail.com>
2022-08-12 18:39:02 -0500

    start of java work

---
+++ utils/generateFile.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+function writeFile(path, content) {
+    // TODO: Create a file in the output folder
+    console.log('I did something');
+}
+
+module.exports = {
+    writeFile
+};
