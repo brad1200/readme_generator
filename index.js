bae0dc491f5684e44caa23221137bb86ebd9cb0e
Brad <bycarlson23@gmail.com>
2022-08-12 21:03:46 -0500

    starting on initial questions i will be using

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Create an array of questions for user input
 const questions = require('./utils/questions');
 const { writeFile } = require('./utils/generateFile');
-
+const inquirer = require('inquirer');
 
 // TODO: Create a function to initialize app
 function init() {
--- utils/questions.js
+++ utils/questions.js
@@ -1,8 +1,27 @@
 const questions = [
     {
         type: 'input',
+        message: 'What is the title for this project?',
         name: 'title',
-        message: 'What is the title for this project'
+        default: 'My ReadMe Generator'
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github',
+        default: 'ex: jake1234'
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+        default: 'ex: carl24@gmail.com'
+    },
+    {
+        type: 'input',
+        message: 'What npm applies to this specific project?',
+        choices: ['inquirer 2.3.4', 'inquirer 8.2.4', 'inquirer 7.6.3', 'none of the above'],
+        name: 'npm'
     }
 ];
 
