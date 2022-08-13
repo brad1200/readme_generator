11642b731f863ab607b61cc3429ba2e754828035
Brad <bycarlson23@gmail.com>
2022-08-12 21:30:45 -0500

    should be final update on questions.js

--- utils/questions.js
+++ utils/questions.js
@@ -3,25 +3,25 @@ const questions = [
         type: 'input',
         message: 'What is the title for this project?',
         name: 'title',
-        default: 'My ReadMe Generator'
+        default: 'ex: My ReadMe Generator'
     },
     {
         type: 'input',
         message: 'Give a brief description of how this project will function.',
         name: 'description',
-        default: 'I will be creating a simple ReadMe.'
+        default: 'ex :I will be creating a simple ReadMe.'
     },
     {
         type: 'input',
         message: 'What needs to be installed for this project to function?',
         name: 'installation',
-        default: 'This? Or that? Or maybe even that??'
+        default: 'ex: This? Or that? Or maybe even that??'
     },
     {
         type: 'input',
         message: 'How will this project be used?',
         name: 'usage',
-        default: 'To help others be able to create a simple ReadMe.'
+        default: 'ex: To help others be able to create a simple ReadMe.'
     },
     {
         type: 'input',
@@ -33,13 +33,13 @@ const questions = [
         type: 'input',
         message: 'List any contributers below if anyone worked on this project with you!',
         name: 'contributers',
-        default: 'Little brother'
+        default: 'ex: Little brother'
     },
     {
         type: 'input',
         message: 'Explain tests used for the project, give detail on how the test work.',
         name: 'tests',
-        default: 'Test 1. Test 2. Test 3.'
+        default: 'ex: Test 1. Test 2. Test 3.'
     },
     {
         type: 'input',
