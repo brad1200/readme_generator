9ac693cf439a44d5ac3cac6471e1dcbd181595da
Brad <bycarlson23@gmail.com>
2022-08-12 21:21:49 -0500

    rewrote questions on how they are supposed to be for the assignment, also based off layout the ReadMe says for the assignment

--- utils/questions.js
+++ utils/questions.js
@@ -7,6 +7,42 @@ const questions = [
     },
     {
         type: 'input',
+        message: 'Give a brief description of how this project will function.',
+        name: 'description',
+        default: 'I will be creating a simple ReadMe.'
+    },
+    {
+        type: 'input',
+        message: 'What needs to be installed for this project to function?',
+        name: 'installation',
+        default: 'This? Or that? Or maybe even that??'
+    },
+    {
+        type: 'input',
+        message: 'How will this project be used?',
+        name: 'usage',
+        default: 'To help others be able to create a simple ReadMe.'
+    },
+    {
+        type: 'input',
+        message: 'What license is going to be used in this project?',
+        name: 'license',
+        choices: ['ISC', 'LLC', 'TRD', 'None of these options']
+    },
+    {
+        type: 'input',
+        message: 'List any contributers below if anyone worked on this project with you!',
+        name: 'contributers',
+        default: 'Little brother'
+    },
+    {
+        type: 'input',
+        message: 'Explain tests used for the project, give detail on how the test work.',
+        name: 'tests',
+        default: 'Test 1. Test 2. Test 3.'
+    },
+    {
+        type: 'input',
         message: 'What is your Github username?',
         name: 'github',
         default: 'ex: jake1234'
@@ -17,12 +53,6 @@ const questions = [
         name: 'email',
         default: 'ex: carl24@gmail.com'
     },
-    {
-        type: 'input',
-        message: 'What npm applies to this specific project?',
-        choices: ['inquirer 2.3.4', 'inquirer 8.2.4', 'inquirer 7.6.3', 'none of the above'],
-        name: 'npm'
-    }
 ];
 
 module.exports = questions;
